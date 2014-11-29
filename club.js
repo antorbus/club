3eba90476c33ac6ac02fe36d68e001d27b177e76
antorbus <antorbus@gmail.com>
2014-11-29 20:08:45 +0000

    cool images

--- club.js
+++ club.js
@@ -1,7 +1,17 @@
 if (Meteor.isClient) {
   Accounts.ui.config({ passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL' });
 
+  Meteor.autorun(function() {
+    if(Meteor.user()) {
+        $('body').css('background-image','url(http://unsplash.imgix.net/uploads/1412594480669535c9ef9/9d85c477?q=75&fm=jpg&s=e511a1f1342208af42aed9515ea97c3e)');
+    }
+    else
+    {
+        $('body').css('background-image','url(http://cdn2.landscapehdwalls.com/wallpapers/1/white-mountains-522-1920x1080.jpg)');
+    }
+  });
 }
+                 
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
