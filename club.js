e5f3677fb197abfaf996a7fb8a6c77a8e2495eda
antorbus <antorbus@gmail.com>
2014-11-29 19:10:34 +0000

    Basic club

---
+++ club.js
@@ -0,0 +1,10 @@
+if (Meteor.isClient) {
+  Accounts.ui.config({ passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL' });
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
